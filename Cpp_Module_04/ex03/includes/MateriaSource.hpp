2d7059944e68c0852389409c6f904c58262b1169
Axel Coézard <axel.coezard@gmail.com>
2021-12-15 09:54:30 +0100

    ajout MateriaSource

--- Cpp_Module_04/ex03/includes/MateriaSource.hpp
+++ Cpp_Module_04/ex03/includes/MateriaSource.hpp
@@ -6,20 +6,28 @@
 /*   By: acoezard <acoezard@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 16:05:45 by acoezard          #+#    #+#             */
-/*   Updated: 2021/12/14 16:13:26 by acoezard         ###   ########.fr       */
+/*   Updated: 2021/12/15 09:44:07 by acoezard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "IMateriaSource.hpp"
+#include "AMateria.hpp"
 
-class MateriaSource
+class MateriaSource : public IMateriaSource
 {
+	private:
+		AMateria	*_learned[4];
 	public:
 		MateriaSource(void);
+		MateriaSource(const MateriaSource & copy);
+		MateriaSource&	operator=(const MateriaSource & copy);
 		~MateriaSource(void);
 
-		void	learnMateria(AMateria*);
+		void	learnMateria(AMateria* m);
 		AMateria*	createMateria(std::string const & type);
+
+		void	setMateria(int i, AMateria	*m);
+		AMateria	*getMateria(int i) const;
 };
--- Cpp_Module_04/ex03/sources/main.cpp
+++ Cpp_Module_04/ex03/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: acoezard <acoezard@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 14:26:13 by acoezard          #+#    #+#             */
-/*   Updated: 2021/12/14 15:59:58 by acoezard         ###   ########.fr       */
+/*   Updated: 2021/12/15 09:54:14 by acoezard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 #include "Character.hpp"
 #include "Cure.hpp"
 #include "Ice.hpp"
+#include "MateriaSource.hpp"
 
 int main(void)
 {
+	MateriaSource	*source = new MateriaSource();
 	Character	*axel = new Character("Axel");
-	Cure	*cure = new Cure();
-	Ice		*ice1 = new Ice();
-	Ice		*ice2 = ice1->clone();
 
-	axel->equip(cure);
-	axel->equip(ice1);
-	axel->equip(ice2);
+	source->learnMateria(new Cure());
+	source->learnMateria(new Ice());
 
-	axel->unequip(1);
+	axel->equip(source->createMateria("ice"));
+	axel->equip(source->createMateria("cure"));
+	axel->equip(source->createMateria("ice"));
+
+	axel->unequip(2);
 
 	axel->use(0, *axel);
 	axel->use(1, *axel);
