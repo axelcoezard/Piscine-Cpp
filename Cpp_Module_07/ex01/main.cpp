0e3b0c93a7a95d08da80c66030d62b064ab6361a
Axel Coézard <axel.coezard@gmail.com>
2022-01-03 18:45:08 +0100

    fix ex07

--- Cpp_Module_07/ex01/main.cpp
+++ Cpp_Module_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: acoezard <acoezard@student.42nice.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 13:04:06 by acoezard          #+#    #+#             */
-/*   Updated: 2021/12/30 13:19:43 by acoezard         ###   ########.fr       */
+/*   Updated: 2022/01/03 18:45:00 by acoezard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ void	put_string(char *str)
 int	main(int ac, char **av)
 {
     std::cout << "Array of char =\n";
-    ::iter<>(av[0], strlen(av[0]), put_char);
+    ::iter(av[0], strlen(av[0]), put_char);
 
     std::cout << "\n\nArray of std::string =\n";
     if (ac > 1)
-        ::iter<>(av + 1, ac - 1, put_string);
+        ::iter(av + 1, ac - 1, put_string);
 
     return (0);
 }
